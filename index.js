ddf7964b80e7e1dbaf0a083cfc2bb91ec5e093de
Bruno Felix Bessa <bruno.felix@grupovab.local>
2022-04-07 18:09:30 -0300

    first comiit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+var Almocar = document.getElementById("b1");
+var email = document.getElementById("b2");
+
+// metodo para mostrar um alerta de treinamento
+function vaitimao() {
+    alert("Formulário enviado com sucesso!" + " " + Almocar.value + " " + email.value);
+}
+
